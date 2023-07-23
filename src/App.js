f1ce74e86e17a7b1d9958cd1cb825e693c9ce936
Rashid Risan <95292841+rashidrzn@users.noreply.github.com>
2023-07-23 20:24:23 +0530

    added the components and assests. And also added the background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBg from "./assests/hot.jpg";
+
+import coldBg from "./assests/cold.jpg";
+// import coldbg from "./assests/cold";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__input">
+            <input type="text" name="city" placeholder="Enter the city...." />
+            <button>*F</button>
+          </div>
+          <div className="section section__temprature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img
+                src="https://home.openweathermap.org/assets/logo_white-12c4f864cc825cfead13b43f6fdae14172bb7848529cb9f48374b9ebb0e9f061.png"
+                alt=""
+              />
+              <h3>Cloudy</h3>
+              <div className="tempreture">
+                <h1>34 *C</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
